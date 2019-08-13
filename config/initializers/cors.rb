9ec717cf583d6254946a6f35a956fae7edd30b34
Luis Rosario <luisrosariomix@gmail.com>
2019-08-13 00:17:05 -0700

    Cors Bug

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -7,7 +7,7 @@
 
 Rails.application.config.middleware.insert_before 0, Rack::Cors do
   allow do
-    origins 'https://react-fitness.herokuapp.com/'
+    origins 'http://react-fitness.herokuapp.com/'
 
     resource '*',
       headers: :any,
