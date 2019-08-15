08588da5ebb6ce87fc3f4b1ee3d4fbcbd86732fa
Luis Rosario <luisrosariomix@gmail.com>
2019-08-14 23:17:56 -0700

    Adjusted Authorize before actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,7 @@
 class UsersController < ApplicationController
-  before_action :authenticate_token, except: [:login, :create]
-  before_action :authorize_user, except: [:login, :check, :destroyCookie, :create, :index]
+  # before_action :authorize_user, except: [:login, :check, :destroyCookie, :create, :index]
   before_action :set_user, only: [:show, :update, :destroy]
+  # before_action :authenticate_token, except: [:login, :create]
 
   # GET /users
   def index
@@ -42,7 +42,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
 
     if @user.save
-      render json: @user, status: :created
+      render json: @user, status: 201
     else
       render json: @user.errors, status: :unprocessable_entity
     end
